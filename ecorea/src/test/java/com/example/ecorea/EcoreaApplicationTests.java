02ccc72f9793977d34e6c00735687c7de395d2d8
haramiee <shrxxv14@gmail.com>
2022-03-22 13:20:37 +0900

    ecorea

---
+++ ecorea/src/test/java/com/example/ecorea/EcoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecorea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
