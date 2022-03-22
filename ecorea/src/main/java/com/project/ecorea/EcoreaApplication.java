36fc5c2919b21039cf6b4fbb0894d10becc1ccf7
haramiee <shrxxv14@gmail.com>
2022-03-22 14:15:04 +0900

    shr
    내 브랜치에 올리고 싶다고 -_-^^^^

---
+++ ecorea/src/main/java/com/project/ecorea/EcoreaApplication.java
@@ -0,0 +1,15 @@
+package com.project.ecorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* 주석 달기 TEST 완료 (●'◡'●) */
+/* 내 브랜치에 올리고 싶다고 -_-^^^^ */
+@SpringBootApplication
+public class EcoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoreaApplication.class, args);
+	}
+
+}
