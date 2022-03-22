5d4d458f96c75d39b75396542eddb27da0f18c37
haramiee <shrxxv14@gmail.com>
2022-03-22 13:58:33 +0900

    shr
    주석 달기 TEST 완료 (●'◡'●)

--- ecorea/src/main/java/com/project/ecorea/EcoreaApplication.java
+++ ecorea/src/main/java/com/project/ecorea/EcoreaApplication.java
@@ -3,6 +3,7 @@ package com.project.ecorea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* 주석 달기 TEST 완료 (●'◡'●) */
 @SpringBootApplication
 public class EcoreaApplication {
 
