719996f8f6b3177c99206dc32345e967a42f2a1f
zzzzuny <wngusdl2647@naver.com>
2022-03-31 23:22:35 +0900

    220331 관심상품 목록 출력, 관심상품 삭제, 관심상품 장바구니에 담기(1/2)

--- ecorea/src/main/java/com/project/ecorea/entity/Bookmark.java
+++ ecorea/src/main/java/com/project/ecorea/entity/Bookmark.java
@@ -1,17 +1,13 @@
 package com.project.ecorea.entity;
 
-
 import lombok.*;
-
+import lombok.experimental.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class Cart {	
+@Accessors(chain = true)
+public class Bookmark {
 	private String memberId;
 	private Integer pno;
-	private Integer cartcnt;
-	private Integer cartprice;
-	private String cartpname;
 }
