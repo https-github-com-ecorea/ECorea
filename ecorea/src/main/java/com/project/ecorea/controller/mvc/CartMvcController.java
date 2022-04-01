16b01ba7369823c053ebeb189af830a55ab83fc5
Jiwoncoding <seojiwonba@gmail.com>
2022-04-01 17:42:13 +0900

    2-2차 프론트 작업

--- ecorea/src/main/java/com/project/ecorea/controller/mvc/CartMvcController.java
+++ ecorea/src/main/java/com/project/ecorea/controller/mvc/CartMvcController.java
@@ -12,16 +12,15 @@ import com.project.ecorea.service.*;
 
 import lombok.*;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Controller
 public class CartMvcController {
-	private CartService cartService;
-	
+	private final CartService cartService;
+	String memberId = "spring11";
 	
 	// 장바구니 보기
 	@GetMapping("/order/cart")
 	public ModelAndView readCart() {
-		String memberId = "zzzzuny";
 		ModelAndView mav = new ModelAndView("order/cart");
 		List<CartDto.CartList> cartList = cartService.readCart(memberId);
 		mav.addObject("cartList", cartList);
@@ -32,7 +31,6 @@ public class CartMvcController {
 	// 장바구니에서 상품 한 개 삭제
 	@PostMapping("/order/cart/deleteOne")
 	public String deleteCartProduct(Integer pno) {
-		String memberId = "zzzzuny";
 		cartService.deleteOne(memberId, pno);
 		return "redirect:/order/cart";
 	}
@@ -40,7 +38,6 @@ public class CartMvcController {
 	// 장바구니에서 상품 전체 삭제
 	@PostMapping("/order/cart/deleteAll")
 	public String deleteAllCartProduct(String confirm) {
-		String memberId = "zzzzuny";
 		if(confirm.equals("confirm")) {
 			cartService.deleteAll(memberId);
 		}		
@@ -50,7 +47,6 @@ public class CartMvcController {
 	// 장바구니에서 선택상품 삭제
 	@PostMapping("/order/cart/deleteSelected")
 	public String deleteSelectedCartProduct(String confirm, CartDto.DeleteSelected dto) {
-		String memberId = "zzzzuny";
 		if(confirm.equals("confirm")) {
 			cartService.deleteSelected(memberId, dto);
 		}
