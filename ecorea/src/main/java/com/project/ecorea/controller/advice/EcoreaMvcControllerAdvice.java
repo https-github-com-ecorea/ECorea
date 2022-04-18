349901f520970b69884d39a276bc761ccaf35a58
zzzzuny <wngusdl2647@naver.com>
2022-04-19 08:45:52 +0900

    220418 controller에 memberId -> principal 로 변경

---
+++ ecorea/src/main/java/com/project/ecorea/controller/advice/EcoreaMvcControllerAdvice.java
@@ -0,0 +1,8 @@
+package com.project.ecorea.controller.advice;
+
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice(basePackages = "com.project.ecorea.controller.mvc")
+public class EcoreaMvcControllerAdvice {
+	
+}
---
+++ ecorea/src/main/java/com/project/ecorea/controller/advice/EcoreaRestControllerAdvice.java
@@ -0,0 +1,8 @@
+package com.project.ecorea.controller.advice;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice(basePackages = "com.project.ecorea.controller.rest")
+public class EcoreaRestControllerAdvice {
+
+}
