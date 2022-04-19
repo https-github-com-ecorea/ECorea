d305e6b1dcc44ae6ff0127bb06eb68bfd75aad6e
haramiee <shrxxv14@gmail.com>
2022-04-19 15:09:15 +0900

    220419 💛 1. 에러 처리 중 ..
    1️⃣ 에러 처리 중 ..

--- ecorea/src/main/java/com/project/ecorea/controller/advice/ExceptionAdvice.java
+++ ecorea/src/main/java/com/project/ecorea/controller/advice/ExceptionAdvice.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@ControllerAdvice(basePackages = "com.project.ecorea.controller.mvc")
-public class MvcAdvice {
+@ControllerAdvice
+public class ExceptionAdvice {
+	
+	/* 메소드의 파라미터나 리턴 값에 문제가 있을 때 오류 (500) */
 	@ExceptionHandler(ConstraintViolationException.class)
 	public String constraintViolationException(ConstraintViolationException e, RedirectAttributes ra) {
 		ra.addFlashAttribute("msg", e.getMessage());
 		return "redirect:/system/error";
 	}
+	
 }
