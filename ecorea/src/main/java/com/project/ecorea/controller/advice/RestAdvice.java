0de06422608ddc72bd7f08cc6e14e42bd3220371
haramiee <shrxxv14@gmail.com>
2022-04-19 09:17:28 +0900

    220419 💛 1. 에러 페이지 처리 중 ..
    1️⃣ 에러 페이지 처리 중 ..

---
+++ ecorea/src/main/java/com/project/ecorea/controller/advice/RestAdvice.java
@@ -0,0 +1,8 @@
+package com.project.ecorea.controller.advice;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "com.project.ecorea.controller.rest")
+public class RestAdvice {
+
+}
